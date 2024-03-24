cf576106264471d443b3bfafad50dbf2875f901f
mopsprivet <115881344+mopsprivet@users.noreply.github.com>
2024-03-24 20:18:33 +0500

    Add files via upload

---
+++ Lab_5/Lab_5/ViewController2.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    var receivedMessage: String?
+
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBAction func backButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton) {
+        // Вызываем метод popViewController для navigationController,
+        // чтобы вернуться на предыдущий экран
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let message = receivedMessage {
+                print(message) 
+            }
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
